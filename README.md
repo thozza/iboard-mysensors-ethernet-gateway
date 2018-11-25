fedaf5858ce5d7f0fa5b4e892d7502ea0c61c8f0
Tomas Hozza <thozza@gmail.com>
2018-11-25 18:11:21 +0100

    Updated firmware for MySensors library version 2.3.0
    Signed-off-by: Tomas Hozza <thozza@gmail.com>

--- README.md
+++ README.md
@@ -1,29 +1,22 @@
+[![Build Status](https://travis-ci.org/thozza/iboard-mysensors-ethernet-gateway.svg?branch=master)](https://travis-ci.org/thozza/iboard-mysensors-ethernet-gateway)
+
 # MySensors Ethernet gateway firmware
 This repository includes the firmware for MySensors Ethernet gateway. The
 firmware is designed for [IBoard](http://wiki.iteadstudio.com/IBoard). For
 building the firmware, the use of [PlatformIO](http://platformio.org)
 tool is assumed.
 
-## Getting the source
-Used libraries are added in this repository as git submodules. Before building
-the firmware it is important to update the git submodules by running:
-```
-git submodule update --init --recursive
-```
+Code expects MySensors library version **2.3.0**.
 
 ## Building the firmware
-Before building the firmware it is important to prepare all the libraries
-into the proper directory structure as expected by PlatformIO. Also it is
-important to replace default settings of some of the libraries with the
-project specific ones. This can be done easily just by running:
-```
-sh prep.sh
-```
-This script will take care of preparing the libraries for PlatformIO. If you
-changed any of the project specific configuration files, it is important to run
-this script again before the build.
-
 Building is easy as running a single command:
 ```
 platformio run
+```
+PlatformIO will take care of downloading MySensors library itself.
+
+## Uploading the firmware
+Run:
+```
+platformio run --target upload
 ```
--- src/iboard-mysensors-ethernet-gateway.ino
+++ src/iboard-mysensors-ethernet-gateway.ino
@@ -1,7 +1,7 @@
 /*
  * iboard-mysensors-ethernet-gateway.ino - Firmware for iboard based MySensors EthernetUDP Gateway
  *
- * Copyright (C) 2014  Tomas Hozza <thozza@gmail.com>
+ * Copyright (C) 2014-2018  Tomas Hozza <thozza@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,145 +23,73 @@
  * nRF24L01+ spec - https://www.sparkfun.com/datasheets/Wireless/Nordic/nRF24L01P_Product_Specification_1_0.pdf
  */
 
-#include <SoftSPI.h>
-#include <MyGateway.h>
-#include <SPI.h>
+#define MY_DEBUG
+#define MY_DEBUG_VERBOSE
+//#define MY_DEBUG_VERBOSE_RF24
+//#define MY_SIGNAL_REPORT_ENABLED
+
+#define MY_RADIO_RF24
+#define MY_GATEWAY_W5100
+
+#define MY_RF24_CE_PIN 3
+#define MY_RF24_CS_PIN 8
+#define MY_RF24_IRQ_PIN 2
+
+/**
+ * - 0 => 2400 Mhz (RF24 channel 1)
+ * - 1 => 2401 Mhz (RF24 channel 2)
+ * - 76 => 2476 Mhz (RF24 channel 77)
+ * - 83 => 2483 Mhz (RF24 channel 84)
+ * - 124 => 2524 Mhz (RF24 channel 125)
+ * - 125 => 2525 Mhz (RF24 channel 126)
+ * @see http://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Frequenzen/Allgemeinzuteilungen/2013_10_WLAN_2,4GHz_pdf.pdf
+ */
+#define MY_RF24_CHANNEL         76             //RF channel for the sensor net, 0-127
+#define MY_RF24_DATARATE        RF24_250KBPS   //RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
+#define MY_RF24_PA_LEVEL 	      RF24_PA_MAX    //Sensor PA Level == RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBM, and RF24_PA_MAX=0dBm
+#define MY_RF24_BASE_RADIO_ID   0x00,0xFC,0xE1,0xA8,0xA8
+
+//#define MY_RX_MESSAGE_BUFFER_FEATURE
+
+#define MY_SOFTSPI
+#define MY_SOFT_SPI_SCK_PIN 7
+#define MY_SOFT_SPI_MISO_PIN 6
+#define MY_SOFT_SPI_MOSI_PIN 5
+
+//#define MY_NODE_ID (AUTO)
+//#define MY_PARENT_NODE_ID (AUTO)
+
+#define MY_IP_RENEWAL_INTERVAL 60*60*1000ul   // in ms
+
+#define MY_PORT 5003
+//#define MY_MAC_ADDRESS 0xDE,0xAD,0xBE,0xEF,0xFE,0xED
+
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+
+#define MY_DEFAULT_ERR_LED_PIN A2  // Error led pin
+#define MY_DEFAULT_RX_LED_PIN  A3  // Receive led pin
+#define MY_DEFAULT_TX_LED_PIN  A4  // Transmit led pin
+
+#define MY_SPLASH_SCREEN_DISABLED
+
 #include <Ethernet.h>
-#include <EthernetUdp.h>
-
-#define SERVER_PORT 5003
-#define IP_RENEWAL_INTERVAL 60000  // in milliseconds
-
-/***********************************/
-/********* PIN DEFINITIONS *********/
-/***********************************/
-#define RF24_CE_pin 3
-#define RF24_CS_pin 8
-
-/*****************************/
-/********* FUNCTIONS *********/
-/*****************************/
-byte *get_random_mac();
-void renewIP();
-void writeEthernet(char *writeEthernet);
-
-/************************************/
-/********* GLOBAL VARIABLES *********/
-/************************************/
-MyGateway gateway(RF24_CE_pin, RF24_CS_pin);
-EthernetUDP server;
-static char recvBuffer[MAX_RECEIVE_LENGTH];
+#include <MySensors.h>
 
 /**********************************/
 /********* IMPLEMENTATION *********/
 /**********************************/
-/**
- * Initialization
- */
-void setup() {
-  Serial.begin(115200);
-  // Configure Ethernet with random MAC address
-  if (Ethernet.begin(get_random_mac()) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-  } else {
-    Serial.print("My IP address: ");
-    Serial.println(Ethernet.localIP());
-  }
-  // give the Ethernet chip second to initialize
-  delay(1000);
 
-  /* configure the UDP server */
-  server.begin(SERVER_PORT);
-  
-  gateway.begin(RF24_PA_LEVEL_GW, RF24_CHANNEL, RF24_DATARATE, writeEthernet);
+void setup()
+{
+  // Setup locally attached sensors
 }
 
-/**
- * Main logic
- */
-void loop() {
-  /* renew the IP address */
-  renewIP();
-
-  int packet_size = server.parsePacket();
-  if (server.available()) {
-   server.read(recvBuffer, MAX_RECEIVE_LENGTH);
-   gateway.parseAndSend(recvBuffer);
-  }
-
-  gateway.processRadioMessage();
+void presentation()
+{
+  // Present locally attached sensors here
 }
 
-/**
- * Send data to the remote controller via UDP
- */
-void writeEthernet(char *writeBuffer) {
-#ifndef GATEWAY_CONTROLLER_DISCOVERY
-  server.write(writeBuffer);
-#else
-  char buffer[16];
-  snprintf_P(buffer, 16, PSTR("0;0;%d;0;%d;"), C_INTERNAL, I_CONTROLLER_DISCOVERY);
-
-  if (strstr(writeBuffer, buffer) != NULL) {
-    /* Controller Discovery msg -> broadcast it! */
-    server.beginPacket(getBroadcastIP(Ethernet.localIP(), Ethernet.subnetMask()), gateway.getControllerPort());
-  }
-  else if (gateway.getControllerIP() == INADDR_NONE) {
-    Serial.println(PSTR("Invalid controller IP - NOT Sending ethernet msg"));
-    return;
-  }
-  else {
-    server.beginPacket(gateway.getControllerIP(), gateway.getControllerPort());
-  }
-  server.write(writeBuffer, strlen(writeBuffer));
-  server.endPacket();
-#endif /* GATEWAY_CONTROLLER_DISCOVERY */
-}
-
-/**
- * Renew the IP address
- */
-void renewIP() {
-  /* renew/rebind IP address
-  0 - nothing happened
-  1 - renew failed
-  2 - renew success
-  3 - rebinf failed
-  4 - rebind success
-  */
-  static unsigned long next_time = millis() + IP_RENEWAL_INTERVAL;
-  unsigned long now = millis();
-
-  // http://playground.arduino.cc/Code/TimingRollover
-  if ((long)(now - next_time) < 0)
-    return;
-
-  if (Ethernet.maintain() & ~(0x06)) {
-    Serial.println("Failed to renew/rebind IP address using DHCP");
-    return;
-  }
-
-  next_time = now + IP_RENEWAL_INTERVAL;
-  Serial.print("My IP address: ");
-  Serial.println(Ethernet.localIP());
-  return;
-}
-
-/**
- * Get random MAC address.
- */
-byte *get_random_mac() {
-  static byte mac[6];
-  mac[0] = 0xDE;
-  mac[1] = 0xAD;
-  mac[2] = 0xBB;
-
-  /* initialize the random generator */
-  randomSeed(analogRead(0));
-
-  for (int i = 3; i < 6; ++i) {
-    mac[i] = random(0x00, 0xFF);
-  }
-
-  return mac;
+void loop()
+{
+  // Send locally attached sensors data here
 }
